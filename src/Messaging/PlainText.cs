050c611c1c283c16fc963388c5928bb83c61a958
Ricky8955555 <397050061@qq.com>
2020-08-19 14:53:52 +0800

    重写 PlainText 的 ToString、Equals 及运算符 ==、!=

--- src/Messaging/PlainText.cs
+++ src/Messaging/PlainText.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using Newtonsoft.Json;
 
 namespace HuajiTech.Mirai.Messaging
 {
@@ -15,12 +16,22 @@ namespace HuajiTech.Mirai.Messaging
         [JsonProperty(PropertyName = "text")]
         public string Content { get; }
 
+        public override string ToString() => Content;
+
+        public override bool Equals(object obj) => obj is PlainText plainText ? plainText.Content == Content : obj as string == Content;
+
+        public override int GetHashCode() => base.GetHashCode();
+
+        public static implicit operator string(PlainText plainText) => plainText.Content ?? string.Empty;
+
+        public static bool operator ==(PlainText left, PlainText right) => left.Equals(right);
+
+        public static bool operator !=(PlainText left, PlainText right) => !left.Equals(right);
+
         /// <summary>
         /// 创建 <see cref="PlainText"/> 实例
         /// </summary>
         /// <param name="content">指定 <see cref="PlainText"/> 实例的内容</param>
         public PlainText(string content) => Content = content;
-
-        public static implicit operator string(PlainText plainText) => plainText.Content ?? string.Empty;
     }
 }
