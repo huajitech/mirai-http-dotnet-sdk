b0906a236024181664e55844042002edb7d5e4cb
Ricky8955555 <397050061@qq.com>
2020-08-28 19:30:40 +0800

    修改文档

--- src/Messaging/CustomElement.cs
+++ src/Messaging/CustomElement.cs
@@ -3,7 +3,7 @@
 namespace HuajiTech.Mirai
 {
     /// <summary>
-    /// 表示自定义消息元素
+    /// 表示自定义消息元素（请使用 <see cref="Newtonsoft.Json"/> 中的特性以指示 <see cref="JsonSerializer"/> 如何处理属性和字段）
     /// </summary>
     public abstract class CustomElement : MessageElement
     {
