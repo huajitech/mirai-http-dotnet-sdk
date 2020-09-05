c90b2c934a44e91fa976052832a399fc3e77e221
Ricky8955555 <397050061@qq.com>
2020-09-05 23:19:49 +0800

    修复 Bug

--- src/Interop/Events/RecallEvents/RecallEventData.cs
+++ src/Interop/Events/RecallEvents/RecallEventData.cs
@@ -1,9 +1,13 @@
-﻿namespace HuajiTech.Mirai.Interop.Events
+﻿using Newtonsoft.Json;
+
+namespace HuajiTech.Mirai.Interop.Events
 {
     internal class RecallEventData
     {
+        [JsonProperty(PropertyName = "authorId")]
         public long AuthorId { get; set; }
 
+        [JsonProperty(PropertyName = "messageId")]
         public int MessageId { get; set; }
     }
 }
