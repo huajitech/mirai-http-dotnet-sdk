53a17bf7a6dc52dcfcc60d63ca9c6f86432b5901
Ricky8955555 <397050061@qq.com>
2020-10-02 00:47:50 +0800

    移除 Chat.SendAsync(Message) 扩展方法

--- src/Extensions/ChatExtensions.cs
+++ src/Extensions/ChatExtensions.cs
@@ -13,14 +13,6 @@ namespace HuajiTech.Mirai.Extensions
         /// <param name="chat">目标聊天</param>
         /// <param name="message">要发送的消息</param>
         /// <returns>所发送消息的 <see cref="Message"/> 实例</returns>
-        public static Task<Message> SendAsync(this Chat chat, Message message) => chat.SendAsync(message.Content);
-
-        /// <summary>
-        /// 异步发送消息到当前 <see cref="Chat"/> 实例
-        /// </summary>
-        /// <param name="chat">目标聊天</param>
-        /// <param name="message">要发送的消息</param>
-        /// <returns>所发送消息的 <see cref="Message"/> 实例</returns>
         public static Task<Message> SendAsync(this Chat chat, MessageElement message) => chat.SendAsync(ComplexMessage.Create(message));
     }
 }
