b953c287e6f450d72b451c97b22e4ffb8a2f3f51
Ricky8955555 <397050061@qq.com>
2020-10-05 00:12:54 +0800

    代码清理

--- src/Chat.cs
+++ src/Chat.cs
@@ -3,7 +3,6 @@ using HuajiTech.Mirai.Messaging;
 using HuajiTech.Mirai.Utilities;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
