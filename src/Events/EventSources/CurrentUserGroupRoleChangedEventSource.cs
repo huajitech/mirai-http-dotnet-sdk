4d59c2918ed81fd1fe988d6fac5c32d7e5b59c46
Ricky8955555 <397050061@qq.com>
2020-12-13 10:26:14 +0800

    补充实现 BotGroupPermissionChangeEvent

---
+++ src/Events/EventSources/CurrentUserGroupRoleChangedEventSource.cs
@@ -0,0 +1,24 @@
+﻿using HuajiTech.Mirai.Http.Interop.Events;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace HuajiTech.Mirai.Http.Events
+{
+    /// <summary>
+    /// 指定当前用户成员角色更改事件源
+    /// </summary>
+    public class CurrentUserGroupRoleChangedEventSource : EventSource<CurrentUserGroupRoleChangedEventArgs>
+    {
+        internal override string Type { get; } = "BotGroupPermissionChangeEvent";
+
+        private protected override async Task<CurrentUserGroupRoleChangedEventArgs> ToEventArgsAsync(string data, Session session)
+        {
+            var eventData = JsonConvert.DeserializeObject<BotGroupPermissionChangeEventData>(data);
+            var group = await eventData.Group.GetGroupAsync(session.CurrentUser);
+            var origin = Member.MemberRoleDictionary[eventData.Origin];
+            var current = Member.MemberRoleDictionary[eventData.Current];
+
+            return new CurrentUserGroupRoleChangedEventArgs(group, origin, current);
+        }
+    }
+}
